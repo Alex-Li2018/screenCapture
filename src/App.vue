6564a02f4c093ed6f1720e25de99a88b9253ecb6
Alex-Li2018 <alexstudyweb@163.com>
2019-01-06 01:39:47 +0800

    网页截屏初版 version 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,261 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="sel">
+      <img src="./1.jpg" class="display" alt="">
+    </div>
+    <button @click="shortcut">截屏</button>
+    <photo-editor v-if="displayFlag" :imgObj="imgObj"></photo-editor>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import html2canvas from "html2canvas"
+  import photoEditor from "./components/photoEditor.js"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      photoEditor
+    },
+    data(){
+      return {
+        displayFlag: false,
+        imgObj: {}, //图片对象
+      }
+    },
+    methods: {
+      //截屏功能
+      shortcut(){
+        var that = this //保存this的指向
+        // 坐标
+        var coord = {
+          leftX0: 0,
+          topY0: 0,
+          leftX1: 0,
+          topY1: 0,
+          leftX2: 0,
+          topY2: 0,
+        }
+        // 尺寸
+        var size = {
+          width: 0,
+          height: 0
+        }
+        // 标志
+        var flag = {
+          mouseMoveFlag: false
+        }
+
+        var createEle = {
+          //创建一个遮罩层
+          createMask() {
+            // 如果有遮罩层了推出程序
+            if( document.querySelector('#mask')) return false
+            var maskDiv = document.createElement("div")
+            //给div设置属性样式
+            maskDiv.id = "mask"
+            maskDiv.style.width = document.body.offsetWidth + "px"
+            maskDiv.style.height = document.body.offsetHeight + "px"
+            maskDiv.style.position = "absolute"
+            maskDiv.style.zIndex = 1000
+            maskDiv.style.left = "0px"
+            maskDiv.style.top = "0px"
+            maskDiv.style.backgroundColor = "#fff"
+            maskDiv.style.opacity = "0.5"
+            
+            //追加到页面上
+            document.body.appendChild(maskDiv)
+
+            //添加事件 
+            document.body.addEventListener("mousedown",eventFN.mouseDown)
+            document.body.addEventListener("mousemove",eventFN.mouseMove)
+            document.body.addEventListener("mouseup",eventFN.mouseUp) 
+            
+          },
+
+          //创建一个区域选择的盒子
+          createSelectDiv() {
+            var div = document.createElement("div")
+            //给div设置属性样式
+            div.className = "short-cut"
+            div.style.width = "0px"
+            div.style.height = "0px"
+            div.style.position = "absolute"
+            div.style.backgroundColor = "#999"
+            div.style.border = "1px solid blue"
+            div.style.position = "absolute"
+            div.style.zIndex = 10000
+
+            //追加到页面上
+            document.body.appendChild(div)
+          },
+
+          //创建确认取消按钮
+          createconfirmBtnAndcancelBtn(){
+            var confrimBtn = document.createElement('button')
+            var cancelBtn = document.createElement('button')
+            // 给button设置样式
+            confrimBtn.innerHTML = "确认"
+            confrimBtn.id = "confrim"
+            confrimBtn.style.width = "50px"
+            confrimBtn.style.height = "30px"
+            confrimBtn.style.position = "absolute"
+            confrimBtn.style.right = "0px"
+            confrimBtn.style.bottom = "-30px"
+
+            cancelBtn.innerHTML = "取消"
+            cancelBtn.id = "cancel"
+            cancelBtn.style.position = "absolute"
+            cancelBtn.style.width = "50px"
+            cancelBtn.style.height = "30px"
+            cancelBtn.style.right =  "50px"
+            cancelBtn.style.bottom = "-30px"
+
+            //追加到页面上
+            document.querySelector(".short-cut").appendChild(confrimBtn)
+            document.querySelector(".short-cut").appendChild(cancelBtn)
+          }
+        }
+
+        var eventFN = {
+          //点击确认事件
+          confrimBtn(event) {
+            if(event.target.id != "confrim") return     
+            //设置参数
+            var opt = {
+              allowTaint: true,
+              scale: 1,
+              height: size.height,
+              width: size.width,
+              x: coord.leftX0,
+              y: coord.topY0
+            }
+            
+            //调用html2canvas的方法生成图片
+            tools.html2canvasFN(opt)
+
+            //删除页面上所有新增的元素
+            document.body.removeChild( document.querySelector('#mask'))
+            document.body.removeChild( document.querySelector('.short-cut'))
+          },
+          
+          //点击取消事件
+          cancelBtn(event) {
+            if(event.target.id != "cancel") return 
+            //删除页面上所有新增的元素
+            document.body.removeChild( document.querySelector('#mask'))
+            document.body.removeChild( document.querySelector('.short-cut'))
+          },
+
+          //鼠标按下事件
+          mouseDown (event) { 
+            // console.log("鼠标按下")
+            if(event.target.id != "mask") return 
+            // 鼠标左键按下
+            if(event.button != 0) return false
+            //判断页面上是否存在了区域选择的框
+            if(document.querySelector('.short-cut')) {
+              // 删除区域选择的盒子
+              document.body.removeChild(document.querySelector('.short-cut'))
+            }
+
+            //开启鼠标移动事件
+            flag.mouseMoveFlag = true
+            
+            //获取鼠标的位置
+            coord.leftX0 = event.pageX
+            coord.topY0 = event.pageY
+
+            //创建选择区域的盒子
+            createEle.createSelectDiv()          
+
+            //设置盒子的定位
+            document.querySelector('.short-cut').style.left = coord.leftX0 + "px"
+            document.querySelector('.short-cut').style.top = coord.topY0 + "px"
+          },
+
+          //鼠标移动事件
+          mouseMove (event) {
+            // console.log("鼠标移动")
+
+            // 如果禁止鼠标移动,就退出函数
+            if(!flag.mouseMoveFlag || event.button != 0) return false
+        
+            // 设置盒子的大小
+            coord.leftX1 = event.pageX
+            coord.topY1 = event.pageY
+            size.width = coord.leftX1 - coord.leftX0
+            size.height = coord.topY1 - coord.topY0
+            //设置盒子的大小
+            document.querySelector('.short-cut').style.width = size.width + "px"
+            document.querySelector('.short-cut').style.height = size.height + "px" 
+          },
+
+          //鼠标弹起事件
+          mouseUp(event) { 
+            // console.log("鼠标弹起")
+            // 鼠标左键按下
+            if(event.button != 0) return false
+            
+            // 设置盒子的大小
+            coord.leftX2 = event.pageX
+            coord.topY2 = event.pageY
+            size.width = coord.leftX2 - coord.leftX0
+            size.height = coord.topY2 - coord.topY0
+            //设置盒子的大小
+            document.querySelector('.short-cut').style.width = size.width + "px"
+            document.querySelector('.short-cut').style.height = size.height + "px" 
+            
+            //关闭鼠标移动事件
+            flag.mouseMoveFlag = false
+
+            //创建确认按钮取消按钮
+            if(document.getElementById('#confrim') || document.getElementById('#cancel')) {
+              document.body.removeChild(document.getElementById('#confrim'))
+              document.body.removeChild(document.getElementById('#cancel'))
+            }
+            //创建确认,取消按钮
+            createEle.createconfirmBtnAndcancelBtn()
+
+            //给确定,取消按钮注册点击事件
+            document.body.addEventListener("click",eventFN.confrimBtn,false)
+            document.body.addEventListener("click",eventFN.cancelBtn,false)
+          },
+        }
+
+        var tools = {
+          // 截图方法
+          html2canvasFN(opt) {
+            html2canvas(document.body,opt).then(function(canvas) {
+              // 图片生成base64
+              var imgURL = canvas.toDataURL()
+              //展示组件
+              that.displayFlag = true
+
+              //传递数据给photoEditor组件
+              let { height,width } = opt
+              that.imgObj = {
+                height,
+                width,
+                imgURL
+              }
+            });
+          }
+        }
+
+        createEle.createMask()
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #sel {
+    margin: 50px 400px; 
+  }
+
+  .display {
+    width: 500px;
+    margin: 50px auto;
+  }
+
 </style>
---
+++ src/components/photoEditor.js
@@ -0,0 +1,468 @@
+import classnames from 'classnames'
+import FabricPhoto from './index'
+import consts from './consts'
+//引入全局样式的方法
+import './scss/index.scss'
+
+// export component
+export default {
+  name: 'wrap-container',
+  props: {
+    imgObj: {
+      type: Object
+    }
+  },
+  created(){
+    // console.log(this.imgObj)
+  },
+  data () {
+    return {
+      editState: consts.states.NORMAL,
+      arrow: {
+        color: '#FF3440',
+        stroke: 4
+      },
+      freeDraw: {
+        color: '#FF3440',
+        stroke: 4
+      },
+      text: {
+        color: '#FF3440'
+      },
+      mosaic: {
+        stroke: '#FF3440'
+      },
+      color: '#FF3440',
+      stroke: '#FF3440'
+    }
+  },
+  methods: {
+    activateShapeMode () {
+      if (this.fp.getCurrentState() !== consts.states.SHAPE) {
+        this.fp.endFreeDrawing()
+        this.fp.endTextMode()
+        this.fp.endLineDrawing()
+        this.fp.endMosaicDrawing()
+        this.fp.endCropping()
+        this.fp.endArrowDrawing()
+        this.fp.endPan()
+        this.fp.startDrawingShapeMode()
+      }
+    },
+    activateTextMode () {
+      if (this.fp.getCurrentState() !== consts.states.TEXT) {
+        this.fp.endFreeDrawing()
+        this.fp.endLineDrawing()
+        this.fp.endArrowDrawing()
+        this.fp.endMosaicDrawing()
+        this.fp.endCropping()
+        this.fp.endDrawingShapeMode()
+        this.fp.endTextMode()
+        this.fp.endPan()
+        this.fp.startTextMode()
+      }
+    },
+    getWindowViewPort () {
+      return {
+        height: $(window).height(),
+        width: $(window).width()
+      }
+    },
+    getDialogViewPort () {
+      const {height, width} = this.getWindowViewPort()
+      return {
+        width: width < 680 ? 680 : width > 900 ? 900 : width,
+        height: height < 450 ? 450 : height > 600 ? 600 : height
+      }
+    },
+    resetEditorState () {
+      this.editState = consts.states.NORMAL
+    },
+    onArrowBtnClick () {
+      this.fp.endAll() //this.fp.startLineDrawing();
+      if (this.editState === consts.states.ARROW) {
+        this.resetEditorState()
+      } else {
+        this.editState = consts.states.ARROW
+        this.fp.startArrowDrawing({
+          width: this.arrow.stroke,
+          color: this.arrow.color
+        })
+      }
+    },
+    onFreeDrawBtnClick () {
+      this.fp.endAll()
+      if (this.editState === consts.states.FREE_DRAWING) {
+        this.resetEditorState()
+      } else {
+        this.editState = consts.states.FREE_DRAWING
+        this.fp.startFreeDrawing({
+          width: this.freeDraw.stroke,
+          color: this.freeDraw.color
+        })
+      }
+    },
+    onMosaicBtnClick () {
+      this.fp.endAll()
+      if (this.editState === consts.states.MOSAIC) {
+        this.resetEditorState()
+      } else {
+        this.editState = consts.states.MOSAIC
+        this.fp.startMosaicDrawing({
+          dimensions: this.mosaic.stroke
+        })
+      }
+    },
+    onTextBtnClick () {
+      if (this.fp.getCurrentState() === consts.states.TEXT) {
+        this.fp.endAll()
+        this.resetEditorState()
+      } else {
+        this.editState = consts.states.TEXT //this.activateTextMode();
+        this.fp.endAll()
+        this.fp.startTextMode()
+      }
+    },
+    onRotationBtnClick () {
+      this.fp.endAll()
+      this.fp.rotate(90)
+      this.resetEditorState()
+    },
+    onCropBtnClick () {
+      this.fp.startCropping()
+    },
+    onClearBtnClick () {
+      this.resetEditorState()
+      this.fp.clearObjects()
+    },
+    onApplyCropBtn () {
+      this.fp.endCropping(true)
+    },
+    onCancleCropBtn () {
+      this.fp.endCropping()
+    },
+    onUndoBtn () {
+      this.fp.undo()
+    },
+    onRedoBtn () {
+      this.fp.redo()
+    },
+    onPanBtnClick () {
+      this.fp.endAll()
+      this.fp.startPan()
+    },
+    renderArrowMenus () {
+      return (
+        <div class="tools-panel">
+          <div class="tools-panel-brush">
+            <div>
+              <span class="small-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+            <div>
+              <span class="normal-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+            <div>
+              <span class="big-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+          </div>
+          <span class="tools-divider"> </span>
+          <div class="tools-panel-color">
+            <span class="color red" onClick={this.changeEditorColor('#FF3440')}>
+              {' '}
+            </span>
+            <span class="color yellow" onClick={this.changeEditorColor('#FFCF50')}>
+              {' '}
+            </span>
+            <span class="color green" onClick={this.changeEditorColor('#00A344')}>
+              {' '}
+            </span>
+            <span class="color blue" onClick={this.changeEditorColor('#0DA9D6')}>
+              {' '}
+            </span>
+            <span class="color grey" onClick={this.changeEditorColor('#999999')}>
+              {' '}
+            </span>
+            <span class="color black" onClick={this.changeEditorColor('#ffffff')}>
+              {' '}
+            </span>
+            <span class="color white" onClick={this.changeEditorColor('#000000')}>
+              {' '}
+            </span>
+          </div>
+        </div>
+      )
+    },
+    renderFreeDrawMenus () {
+      return (
+        <div class="tools-panel">
+          <div class="tools-panel-brush">
+            <div>
+              <span class="small-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+            <div>
+              <span class="normal-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+            <div>
+              <span class="big-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+          </div>
+          <span class="tools-divider"> </span>
+          <div class="tools-panel-color">
+            <span class="color red" onClick={this.changeEditorColor('#FF3440')}>
+              {' '}
+            </span>
+            <span class="color yellow" onClick={this.changeEditorColor('#FFCF50')}>
+              {' '}
+            </span>
+            <span class="color green" onClick={this.changeEditorColor('#00A344')}>
+              {' '}
+            </span>
+            <span class="color blue" onClick={this.changeEditorColor('#0DA9D6')}>
+              {' '}
+            </span>
+            <span class="color grey" onClick={this.changeEditorColor('#999999')}>
+              {' '}
+            </span>
+            <span class="color black" onClick={this.changeEditorColor('#ffffff')}>
+              {' '}
+            </span>
+            <span class="color white" onClick={this.changeEditorColor('#000000')}>
+              {' '}
+            </span>
+          </div>
+        </div>
+      )
+    },
+    renderMosaicMenus () {
+      return (
+        <div class="tools-panel">
+          <div class="tools-panel-brush">
+            <div>
+              <span class="small-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+            <div>
+              <span class="normal-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+            <div>
+              <span class="big-brush" onClick={() => {}}>
+                {' '}
+              </span>
+            </div>
+          </div>
+        </div>
+      )
+    },
+    renderTextMenus () {
+      return (
+        <div class="tools-panel">
+          <div class="tools-panel-color">
+            <span class="color red" onClick={this.changeEditorColor('#FF3440')}>
+              {' '}
+            </span>
+            <span class="color yellow" onClick={this.changeEditorColor('#FFCF50')}>
+              {' '}
+            </span>
+            <span class="color green" onClick={this.changeEditorColor('#00A344')}>
+              {' '}
+            </span>
+            <span class="color blue" onClick={this.changeEditorColor('#0DA9D6')}>
+              {' '}
+            </span>
+            <span class="color grey" onClick={this.changeEditorColor('#999999')}>
+              {' '}
+            </span>
+            <span class="color black" onClick={this.changeEditorColor('#ffffff')}>
+              {' '}
+            </span>
+            <span class="color white" onClick={this.changeEditorColor('#000000')}>
+              {' '}
+            </span>
+          </div>
+        </div>
+      )
+    },
+    renderCropMenus () {
+      return (
+        <div class="tools-panel">
+          <div class="tools-panel-crop">
+            <span class="tools-panel-crop-apply-btn" onClick={this.onApplyCropBtn.bind(this)}>
+              {' '}
+            </span>
+            <span class="tools-panel-crop-cancel-btn" onClick={this.onCancleCropBtn.bind(this)}>
+              {' '}
+            </span>
+          </div>
+        </div>
+      )
+    },
+    zoomOut (delta) {
+      let nextZoom = this.fp.getZoom() + delta
+      if (nextZoom > 4) {
+        return
+      }
+      this.fp.setZoom(nextZoom)
+    },
+    zoomIn (delta) {
+      let nextZoom = this.fp.getZoom() - delta
+      if (nextZoom < 1) {
+        return
+      }
+      this.fp.setZoom(nextZoom)
+    },
+    changeEditorColor () {
+      return () => {}
+    },
+    onURL () {
+      this.fp.toDataURL('image/png')
+    }
+  },
+  mounted () {
+    window.fabricPhoto = this.fp = new FabricPhoto('#upload-file-image-preview', {
+      cssMaxWidth: this.imgObj.width,
+      cssMaxHeight: this.imgObj.height
+    })
+    debugger
+    this.fp.once('loadImage', oImage => {
+      this.fp.clearUndoStack()
+    }) 
+    // 传入图片的base64
+    this.fp.loadImageFromURL(this.imgObj.imgURL, 'image name') 
+    
+    this.fp.on('selectObject', obj => {
+      //console.log('selectObject--->',obj);
+      if (obj.type === 'rect' || obj.type === 'circle' || obj.type === 'triangle') {
+        this.editState = consts.states.SHAPE
+        this.activateShapeMode()
+      } else if (obj.type === 'text') {
+        this.editState = consts.states.TEXT
+        this.activateTextMode()
+      }
+    })
+    this.fp.on('activateText', obj => {
+      //console.log('activateText----obj--->',obj);
+      // add new text on cavas
+      if (obj.type === 'new') {
+        console.log('--activateText--new-->', obj)
+        this.fp.addText('双击编辑', {
+          styles: {
+            fill: this.text.color,
+            fontSize: 50
+          },
+          position: obj.originPosition
+        })
+      }
+    })
+    this.fp.on({
+      emptyUndoStack: () => {
+        // $btnUndo.addClass('disabled');
+        // resizeEditor();
+      },
+      emptyRedoStack: () => {
+        // $btnRedo.addClass('disabled');
+        // resizeEditor();
+      },
+      pushUndoStack: () => {
+        // $btnUndo.removeClass('disabled');
+        // resizeEditor();
+      },
+      pushRedoStack: () => {
+        // $btnRedo.removeClass('disabled');
+        // resizeEditor();
+      },
+      endCropping: () => {
+        // $cropSubMenu.hide();
+        // resizeEditor();
+      },
+      endFreeDrawing: () => {
+        //$freeDrawingSubMenu.hide();
+      },
+      adjustObject: (obj, type) => {
+        if (obj.type === 'text' && type === 'scale') {
+          //$inputFontSizeRange.val(obj.getFontSize());
+        }
+      }
+    })
+  },
+  destroyed () {
+    if (this.fp) {
+      // this.fp.destory()
+      this.fp = null
+      $('#upload-file-image-preview-paper').empty()
+    }
+  },
+  render () {
+    let btnClassname = classnames({
+      'file-button': true,
+      'file-button--pc': process.env.APP_ENV === 'pc',
+      'upload-success': true
+    })
+    let menus = null
+    this.fp && console.log('editor state', this.fp.getCurrentState())
+    if (this.fp && this.fp.getCurrentState() === consts.states.FREE_DRAWING) {
+      menus = this.renderFreeDrawMenus()
+    } else if (this.fp && this.fp.getCurrentState() === consts.states.ARROW) {
+      menus = this.renderArrowMenus()
+    } else if (this.fp && this.fp.getCurrentState() === consts.states.MOSAIC) {
+      menus = this.renderMosaicMenus()
+    } else if (this.fp && this.fp.getCurrentState() === consts.states.TEXT) {
+      menus = this.renderTextMenus()
+    } else {
+      menus = null
+    }
+    return (
+      <div class="wrap_inner">
+        <div class="main">
+          <div class="upload-file-image-preview" id="upload-file-image-preview" />
+          <div class={btnClassname}>
+            <div class="image-thumb-btns">
+              <i class="dxicon dxicon-image-suoxiao" onClick={this.zoomIn.bind(this, 0.2)} />
+              <div class="thumb-divider" />
+              <i class="dxicon dxicon-image-fangda" onClick={this.zoomOut.bind(this, 0.2)} />
+            </div>
+            <div class="image-tools-btns">
+              <i class="dxicon dxicon-image-jiantou" onClick={this.onArrowBtnClick.bind(this)} />
+              <i class="dxicon dxicon-image-huabi" onClick={this.onFreeDrawBtnClick.bind(this)} />
+              <i class="dxicon dxicon-image-text" onClick={this.onTextBtnClick.bind(this)} />
+              <i class="dxicon dxicon-image-masaike" onClick={this.onMosaicBtnClick.bind(this)} />
+              <i class="dxicon dxicon-image-xuanzhuan" onClick={this.onRotationBtnClick.bind(this)} />
+              <i class="dxicon dxicon-image-jiancai" onClick={this.onCropBtnClick.bind(this)} />
+              <i class="dxicon dxicon-image-jiancai" onClick={this.onPanBtnClick.bind(this)} />
+              <span class="tools-divider"> </span>
+              <span class="file-button-cancel" onClick={this.onClearBtnClick.bind(this)}>
+                复原
+              </span>
+              <span class="file-button-cancel" onClick={this.onUndoBtn.bind(this)}>
+                undo
+              </span>
+              <span class="file-button-cancel" onClick={this.onRedoBtn.bind(this)}>
+                redo
+              </span>
+              <span class="file-button-cancel" onClick={this.onURL.bind(this)}>
+                url
+              </span>
+              {menus}
+            </div>
+            <div class="ctn-btns" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
